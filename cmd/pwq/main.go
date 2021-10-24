c90e66056ed4d87fce4fd37fb249deb95cf4ffd8
Philipp Böschen <boeschenphilipp@gmail.com>
2021-10-24 21:28:53 +0200

    First working scaler by examining growth at intervals

---
+++ cmd/pwq/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var workerCount int32
+
+func main()  {
+	q := make(chan string, 1)
+	wg := sync.WaitGroup{}
+
+	for i:=0; i < 1; i++ {
+		wg.Add(1)
+		atomic.AddInt32(&workerCount, 1)
+		go worker(&wg, q)
+	}
+
+	go watchAndScale(context.Background(), QueueWatcher{
+		Queue:         q,
+		QueueWG:       &wg,
+	})
+
+	for i := 0; i < 100; i++ {
+		time.Sleep(100 * time.Millisecond)
+		q <- fmt.Sprintf("message %d", i)
+		fmt.Printf("Queuedepth: %d\n", len(q))
+	}
+
+	close(q)
+
+	wg.Wait()
+}
+
+func worker(wg *sync.WaitGroup, workChan chan string) {
+	for s := range workChan {
+		time.Sleep(1 * time.Second)
+		fmt.Println(s)
+	}
+	wg.Done()
+}
+
+type QueueWatcher struct {
+	Queue chan string
+	QueueWG *sync.WaitGroup
+	PreviousDepth int
+	CurrentDepth int
+}
+
+func watchAndScale(ctx context.Context, watcher QueueWatcher) {
+	ticker := time.NewTicker(50 * time.Millisecond)
+
+	for {
+		select {
+		case <- ctx.Done():
+			fmt.Println(ctx.Err())
+		case <- ticker.C:
+			watcher.CurrentDepth = len(watcher.Queue)
+
+			// < 0 means shrinking and > 0 means growing
+			// we need to add workers when it's > 0
+			growthTrend := watcher.CurrentDepth - watcher.PreviousDepth
+			if growthTrend > 0 {
+				watcher.QueueWG.Add(1)
+				atomic.AddInt32(&workerCount, 1)
+				go worker(watcher.QueueWG, watcher.Queue)
+				fmt.Printf("added worker for a total of %d workers\n", workerCount)
+			}
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/deichindianer/playing-with-queues
+
+go 1.17
