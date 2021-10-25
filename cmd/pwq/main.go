bd2378fbc24894e6184980102614288cb9e83739
Philipp Böschen <boeschenphilipp@gmail.com>
2021-10-25 14:31:17 +0200

    Add stupid test case

--- cmd/pwq/main.go
+++ cmd/pwq/main.go
@@ -11,7 +11,7 @@ import (
 var workerCount int32
 
 func main()  {
-	q := make(chan string, 1)
+	q := make(chan string, 10)
 	wg := sync.WaitGroup{}
 
 	for i:=0; i < 1; i++ {
@@ -27,6 +27,12 @@ func main()  {
 
 	for i := 0; i < 100; i++ {
 		time.Sleep(100 * time.Millisecond)
+		q <- fmt.Sprintf("message %d", i)
+		fmt.Printf("Queuedepth: %d\n", len(q))
+	}
+
+	for i := 0; i < 100; i++ {
+		time.Sleep(10 * time.Millisecond)
 		q <- fmt.Sprintf("message %d", i)
 		fmt.Printf("Queuedepth: %d\n", len(q))
 	}
@@ -64,12 +70,17 @@ func watchAndScale(ctx context.Context, watcher QueueWatcher) {
 			// < 0 means shrinking and > 0 means growing
 			// we need to add workers when it's > 0
 			growthTrend := watcher.CurrentDepth - watcher.PreviousDepth
+			fmt.Printf("Growthtrend: %d\n", growthTrend)
 			if growthTrend > 0 {
 				watcher.QueueWG.Add(1)
 				atomic.AddInt32(&workerCount, 1)
 				go worker(watcher.QueueWG, watcher.Queue)
 				fmt.Printf("added worker for a total of %d workers\n", workerCount)
 			}
+			if growthTrend < 0 {
+				fmt.Println("would shrink workers by 1")
+			}
+			watcher.PreviousDepth = watcher.CurrentDepth
 		}
 	}
 }
